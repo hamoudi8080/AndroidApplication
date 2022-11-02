973b44350c6183ab39f85f5057ef05acf0239f57
hamoudi8080 <hamoderaso@outlook.com>
2022-11-02 20:12:08 +0100

    time buttons has been created with new acitvity class

--- app/src/main/java/com/example/minsuapplication/MainActivity.java
+++ app/src/main/java/com/example/minsuapplication/MainActivity.java
@@ -3,10 +3,19 @@ package com.example.minsuapplication;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.app.TimePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TimePicker;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button newShiftButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,8 +25,14 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.my_toolbar);
         setSupportActionBar(toolbar);
 
-        //HELLO
-        //you
+        newShiftButton = findViewById(R.id.new_shiftButton);
+
+        newShiftButton.setOnClickListener(view -> {
+            Intent intent = new Intent(this, NewShiftActivity.class);
+            startActivity(intent);
+        });
+
+
     }
 
     @Override
@@ -25,4 +40,64 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.main_menu, menu);
         return super.onCreateOptionsMenu(menu);
     }
+
+
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        getMenuInflater().inflate(R.menu.main_menu, menu);
+//        return super.onCreateOptionsMenu(menu);
+//    }
+//
+//    public void startTime(View view) {
+//
+//        TimePickerDialog.OnTimeSetListener onTimeSetListener= new TimePickerDialog.OnTimeSetListener(){
+//
+//            @Override
+//            public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
+//                hour = selectedHour;
+//                minute = selectedMinute;
+//                startTimeButton.setText(String.format(Locale.getDefault(),"%02d:%02d", hour, minute));
+//            }
+//        };
+//        TimePickerDialog timePickerDialog = new TimePickerDialog(this, onTimeSetListener, hour, minute, true);
+//        timePickerDialog.setTitle("Select Time");
+//        timePickerDialog.show();
+//    }
+
+
+
+
+//
+//
+//    Button startTimeButton;
+//    Button endTimeButton;
+//    int hour, minute;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.new_shift);
+//
+//        startTimeButton = findViewById(R.id.selectTime);
+//        endTimeButton = findViewById(R.id.selectTime2);
+//    }
+//
+//
+//
+//    public void startTime(View view) {
+//
+//        TimePickerDialog.OnTimeSetListener onTimeSetListener = new TimePickerDialog.OnTimeSetListener(){
+//
+//            @Override
+//            public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
+//                hour = selectedHour;
+//                minute = selectedMinute;
+//                startTimeButton.setText(String.format(Locale.getDefault(),"%02d:%02d", hour, minute));
+//            }
+//        };
+//        TimePickerDialog timePickerDialog = new TimePickerDialog(this, onTimeSetListener, hour, minute, true);
+//        timePickerDialog.setTitle("Select Time");
+//        timePickerDialog.show();
+//    }
 }
---
+++ app/src/main/java/com/example/minsuapplication/NewShiftActivity.java
@@ -0,0 +1,63 @@
+package com.example.minsuapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TimePicker;
+
+import java.util.Locale;
+
+public class NewShiftActivity extends AppCompatActivity {
+
+
+
+    Button startTimeButton;
+    Button endTimeButton;
+    private int hour;
+    private int minute;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.new_shift);
+
+        startTimeButton = findViewById(R.id.selectTime);
+        endTimeButton = findViewById(R.id.selectTime2);
+    }
+
+
+
+    public void startTime(View view) {
+
+        TimePickerDialog.OnTimeSetListener onTimeSetListener = new TimePickerDialog.OnTimeSetListener(){
+
+            @Override
+            public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
+                hour = selectedHour;
+                minute = selectedMinute;
+                startTimeButton.setText(String.format(Locale.getDefault(),"%02d:%02d", hour, minute));
+            }
+        };
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this, onTimeSetListener, hour, minute, true);
+        timePickerDialog.setTitle("Select Time");
+        timePickerDialog.show();
+    }
+
+    public void endTime(View view) {
+
+        TimePickerDialog.OnTimeSetListener onTimeSetListener = new TimePickerDialog.OnTimeSetListener(){
+
+            @Override
+            public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
+                hour = selectedHour;
+                minute = selectedMinute;
+                endTimeButton.setText(String.format(Locale.getDefault(),"%02d:%02d", hour, minute));
+            }
+        };
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this, onTimeSetListener, hour, minute, true);
+        timePickerDialog.setTitle("Select Time");
+        timePickerDialog.show();
+    }
+}
