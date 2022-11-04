850b373cd1fc23419f4a5cb0bd00b8256b93b8a0
hamoudi8080 <hamoderaso@outlook.com>
2022-11-04 21:51:21 +0100

    upButton is added and floating button is also added with navbar for new shift activity

--- app/src/main/java/com/example/minsuapplication/MainActivity.java
+++ app/src/main/java/com/example/minsuapplication/MainActivity.java
@@ -43,61 +43,9 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        getMenuInflater().inflate(R.menu.main_menu, menu);
-//        return super.onCreateOptionsMenu(menu);
-//    }
-//
-//    public void startTime(View view) {
-//
-//        TimePickerDialog.OnTimeSetListener onTimeSetListener= new TimePickerDialog.OnTimeSetListener(){
-//
-//            @Override
-//            public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
-//                hour = selectedHour;
-//                minute = selectedMinute;
-//                startTimeButton.setText(String.format(Locale.getDefault(),"%02d:%02d", hour, minute));
-//            }
-//        };
-//        TimePickerDialog timePickerDialog = new TimePickerDialog(this, onTimeSetListener, hour, minute, true);
-//        timePickerDialog.setTitle("Select Time");
-//        timePickerDialog.show();
-//    }
 
 
 
 
-//
-//
-//    Button startTimeButton;
-//    Button endTimeButton;
-//    int hour, minute;
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.new_shift);
-//
-//        startTimeButton = findViewById(R.id.selectTime);
-//        endTimeButton = findViewById(R.id.selectTime2);
-//    }
-//
-//
-//
-//    public void startTime(View view) {
-//
-//        TimePickerDialog.OnTimeSetListener onTimeSetListener = new TimePickerDialog.OnTimeSetListener(){
-//
-//            @Override
-//            public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
-//                hour = selectedHour;
-//                minute = selectedMinute;
-//                startTimeButton.setText(String.format(Locale.getDefault(),"%02d:%02d", hour, minute));
-//            }
-//        };
-//        TimePickerDialog timePickerDialog = new TimePickerDialog(this, onTimeSetListener, hour, minute, true);
-//        timePickerDialog.setTitle("Select Time");
-//        timePickerDialog.show();
-//    }
+
 }
--- app/src/main/java/com/example/minsuapplication/NewShiftActivity.java
+++ app/src/main/java/com/example/minsuapplication/NewShiftActivity.java
@@ -1,12 +1,19 @@
 package com.example.minsuapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.annotation.SuppressLint;
 import android.app.TimePickerDialog;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TimePicker;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.Locale;
 
@@ -19,15 +26,38 @@ public class NewShiftActivity extends AppCompatActivity {
     Button endTimeButton;
     private int hour;
     private int minute;
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.new_shift);
+        Toolbar toolbar = findViewById(R.id.my_toolbar);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        FloatingActionButton floatingActionButton = findViewById(R.id.flotbutton);
+
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(NewShiftActivity.this, "SETTINGS WAS CLICKED", Toast.LENGTH_SHORT).show();
+            }
+        });
+
 
         startTimeButton = findViewById(R.id.selectTime);
         endTimeButton = findViewById(R.id.selectTime2);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
 
     public void startTime(View view) {
