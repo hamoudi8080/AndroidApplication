a428072e6e30c53e84777e635a87481c218d017b
hamoudi8080 <hamoderaso@outlook.com>
2022-11-09 15:53:36 +0100

    data picker added

--- app/src/main/java/com/example/minsuapplication/NewShiftActivity.java
+++ app/src/main/java/com/example/minsuapplication/NewShiftActivity.java
@@ -4,56 +4,85 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.Calendar;
 import java.util.Locale;
 
 public class NewShiftActivity extends AppCompatActivity {
 
 
-
-
-    Button startTimeButton;
-    Button endTimeButton;
+    private Button startTimeButton;
+    private Button endTimeButton;
     private int hour;
     private int minute;
 
+    private DatePickerDialog datePickerDialog;
+
+    private Button dateButton;
+    private Button dateButton2;
+
+    private int year;
+    private int month;
+    private int day;
+
+    private String date;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.new_shift);
+//       getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("SU CALCULATION APP");
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         FloatingActionButton floatingActionButton = findViewById(R.id.flotbutton);
-
         floatingActionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(NewShiftActivity.this, "SETTINGS WAS CLICKED", Toast.LENGTH_SHORT).show();
+                Toast.makeText(NewShiftActivity.this, date , Toast.LENGTH_SHORT).show();
             }
         });
 
 
+        intitDatePicker();
+        dateButton = findViewById(R.id.set_date);
+        dateButton.setText(getTodaysDate());
+
+        dateButton2 = findViewById(R.id.set_date2);
+        dateButton2.setText(getTodaysDate());
+
+
+
         startTimeButton = findViewById(R.id.selectTime);
         endTimeButton = findViewById(R.id.selectTime2);
+
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main_menu, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
+  private String getTodaysDate(){
+
+      Calendar cal = Calendar.getInstance();
+      int year = cal.get(Calendar.YEAR);
+      int month = cal.get(Calendar.MONTH);
+
+      month = month +1;
+      int day = cal.get(Calendar.DAY_OF_MONTH);
+      return makeDateString(day, month,year);
+  }
 
 
     public void startTime(View view) {
@@ -87,4 +116,84 @@ public class NewShiftActivity extends AppCompatActivity {
         timePickerDialog.setTitle("Select Time");
         timePickerDialog.show();
     }
+
+    private void intitDatePicker(){
+         DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                month = month + 1;
+                  date = makeDateString(day, month, year);
+                dateButton.setText(date);
+                dateButton2.setText(date);
+            }
+        };
+        Calendar cal = Calendar.getInstance();
+         int year = cal.get(Calendar.YEAR);
+         int month = cal.get(Calendar.MONTH);
+         int day = cal.get(Calendar.DAY_OF_MONTH);
+
+        int style = AlertDialog.THEME_HOLO_LIGHT;
+        datePickerDialog = new DatePickerDialog(this, style, dateSetListener, year, month, day);
+    }
+
+
+
+    private String makeDateString(int year, int month, int day){
+        this.year= year;
+        this.month = month;
+        this.day = day;
+        return getMonthFormat(month) + " " + day + " " + year;
+    }
+
+
+
+    private String getMonthFormat(int month){
+        if(month == 1)
+            return "JAN";
+        if(month == 2)
+            return "FEB";
+        if(month == 3)
+            return "MAR";
+        if(month == 4)
+            return "APR";
+        if(month == 5)
+            return "MAY";
+        if(month == 6)
+            return "JUN";
+        if(month == 7)
+            return "JUL";
+        if(month == 8)
+            return "AUG";
+        if(month == 9)
+            return "SEP";
+        if(month == 10)
+            return "OCT";
+        if(month == 11)
+            return "NOV";
+        if(month == 12)
+            return "DEC";
+
+        //default should never happen
+        return "JAN";
+    }
+
+    public void setDate(View view) {
+
+        datePickerDialog.show();
+
+    }
+
+
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+
+
 }
